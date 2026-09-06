f5d17d6fc78428589e5288408632f4d6f44bb936
allensumaden <absumaden@up.edu.ph>
2026-09-07 00:39:11 +0800

    refactor app layout and error handling styles

--- My First App/app.py
+++ My First App/app.py
@@ -1,12 +1,13 @@
 import webbrowser
+
 import dash
 import dash_bootstrap_components as dbc
-from dash import dcc, html
-
 from dash import dcc, html, Input, Output, State, callback
 from dash.exceptions import PreventUpdate
 
-app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
+from utilities import generateFibonacci, getFactorial
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.title = "My First Dash App"
 
@@ -14,7 +15,7 @@ app.layout = html.Div(
     [
         dbc.Card(
             [
-                dbc.CardHeader("First Dash App"),
+                dbc.CardHeader("First Dash App", className='app-header'),
                 dbc.CardBody(
                     [
                         html.Div(
@@ -23,7 +24,7 @@ app.layout = html.Div(
                                 dbc.Input(placeholder="Place a number here", type="text", id='num_input'),
                                 dbc.FormText("Negative numbers are not allowed."),
                             ]
-                        ), 
+                        ),
                         html.Div(
                             [
                                 dbc.Label("Process"),
@@ -36,13 +37,14 @@ app.layout = html.Div(
                                 ),
                                 dbc.FormText("Select an operation"),
                             ]
-                        ), 
-                        dbc.Button("Calculate!", id='btn_calculate', color='primary', n_clicks=0),
+                        ),
+                        dbc.Button("Calculate!", id='btn_calculate', className='calc-btn', n_clicks=0),
                         html.Div(id='output_area')
                     ]
                 ),
             ],
-            style={"width": "18rem"},
+            className='app-card',
+            style={"width": "20rem"},
         )
     ],
     style={
@@ -53,7 +55,6 @@ app.layout = html.Div(
     },
 )
 
-from utilities import generateFibonacci, getFactorial
 
 @app.callback(
     [
@@ -61,9 +62,9 @@ from utilities import generateFibonacci, getFactorial
     ],
     [
         Input('btn_calculate', 'n_clicks')
-    ], 
+    ],
     [
-        State('num_input', 'value'), 
+        State('num_input', 'value'),
         State('process_select', 'value')
     ]
 )
@@ -76,38 +77,35 @@ def calculateResults(btncalculate_clicks, num_input, process_select):
             try:
                 float_val = float(num_input)
             except (ValueError, TypeError):
-                return [html.Span("Words are not allowed.", style={"color": "red"})]
+                return [html.Span("Words are not allowed.", className='error-text')]
 
             if not float_val.is_integer():
-                return [html.Span("Integers only.", style={"color": "red"})]
+                return [html.Span("Integers only.", className='error-text')]
 
             valid_num = int(float_val)
-
             if valid_num < 0:
-                return [html.Span("No negative numbers.", style={"color": "red"})]
+                return [html.Span("No negative numbers.", className='error-text')]
 
             process_select = int(process_select)
 
             if process_select == 1:
                 factorial_value = getFactorial(valid_num)
-                output_val = ("The factorial is "+ str(factorial_value) + ".")
-
+                output_val = "The factorial is " + str(factorial_value) + "."
             elif process_select == 2:
-                    fib_sequence = generateFibonacci(valid_num)
-                    fib_sequence_str = [str(i) for i in fib_sequence]
-                    output_seq = ", ".join(fib_sequence_str)
-                    output_val = ("We get the sequence "+ output_seq + ".")
-
+                fib_sequence = generateFibonacci(valid_num)
+                fib_sequence_str = [str(i) for i in fib_sequence]
+                output_seq = ", ".join(fib_sequence_str)
+                output_val = "We get the sequence " + output_seq + "."
             else:
                 raise PreventUpdate
 
-            return [html.Span(output_val, style={"color": "black"})]
+            return [html.Span(output_val, className='result-text')]
 
         except Exception:
-            return [html.Span("Unknown input, try again!", style={"color": "red"})]
-
+            return [html.Span("Unknown input, try again!", className='error-text')]
     else:
         raise PreventUpdate
+
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:8050', autoraise=True)
